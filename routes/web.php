b98c75a9aeafd49c29d41ff3877443eda78ffea0
wanghaotianGitHub <695916225@qq.com>
2019-09-19 07:35:07 +0000

    无用提交

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,3 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-//Route::get('/home', 'HomeController@index')->name('home');
-//袁帅最帅    
-//最帅
