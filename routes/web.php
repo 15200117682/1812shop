dcb5db54e6317f67f816d5bbf5f2fa7724544e95
Jack <haiwanlvzhu@163.com>
2019-09-19 08:25:20 +0000

    支付同步完成

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,9 @@ Route::get('/home', 'HomeController@index')->name('home');
 //Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/','index\IndexController@index');//前台首页
+
+//崔健
+Route::prefix('alipay')->group(function(){
+	Route::get('pay','Alipay\AlipayController@pay'); //支付方法
+	Route::get('return_url','Alipay\AlipayController@aliReturn');//同步通知
+});
