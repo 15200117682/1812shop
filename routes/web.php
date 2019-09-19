28aa7391341f2c677d056698ae6fc3e22397afd8
dx <1261335491@qq.com>
2019-09-19 07:27:39 +0000

    父模板

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 //Route::get('/home', 'HomeController@index')->name('home');
+//
+
+Route::get('/','index\IndexController@index');//前台首页
