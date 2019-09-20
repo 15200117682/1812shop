0cf9ac73f454a83e5b72d8136af78200ec3db15f
yuanfen <yuanfen@localhost.localdomain>
2019-09-20 03:55:00 +0000

    后台商品管理

--- app/Model/BrandModel.php
+++ app/Model/BrandModel.php
@@ -3,9 +3,21 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Encore\Admin\Traits\ModelTree;
+
 
 class BrandModel extends Model
 {
+    use ModelTree;
     protected $table="brand";
     public $primaryKey="b_id";
+
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        $this->setParentColumn('status');
+        $this->setOrderColumn('u_time');
+        $this->setTitleColumn('b_name');
+    }
 }
